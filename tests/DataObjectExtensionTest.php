26a4346f71ab20e2a6419937c708f07422aa2eae
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2026-03-19 18:04:25 +1100

    Add a unit test

---
+++ tests/DataObjectExtensionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace NSWDPC\DataObjectEditable\Tests;
+
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Security\Member;
+
+class DataObjectExtensionTest extends SapphireTest
+{
+
+    protected $usesDatabase = true;
+
+    protected static $fixture_file = './DataObjectExtensionTest.yml';
+
+    protected static $extra_dataobjects = [
+        TestDataObject::class,
+    ];
+
+    public function testPermissions(): void
+    {
+        $user = $this->objFromFixture(Member::class, 'memberwith');
+        $this->loginAs($user);
+        $test = TestDataObject::create(['Title' => 'Test']);
+        $this->assertTrue($test->canCreate($user));
+        $this->assertTrue($test->canEdit($user));
+        $this->assertTrue($test->canDelete($user));
+        $this->assertTrue($test->canView($user));
+    }
+
+    public function testNegatedPermissions(): void
+    {
+        $user = $this->objFromFixture(Member::class, 'memberwithout');
+        $this->loginAs($user);
+        $test = TestDataObject::create(['Title' => 'Test negated']);
+        $this->assertFalse($test->canCreate($user));
+        $this->assertFalse($test->canEdit($user));
+        $this->assertFalse($test->canDelete($user));
+        $this->assertFalse($test->canView($user));
+    }
+}
---
+++ tests/TestDataObject.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace NSWDPC\DataObjectEditable\Tests;
+
+use NSWDPC\DataObjectEditable\Extensions\DataObjectExtension;
+use SilverStripe\Dev\TestOnly;
+use SilverStripe\ORM\DataObject;
+
+class TestDataObject extends DataObject implements TestOnly
+{
+
+    private static array $db = [
+        'Title' => 'Varchar',
+    ];
+
+    private static array $extensions = [
+        DataObjectExtension::class
+    ];
+
+}
